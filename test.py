34317ea714758c08dc4532bf5884587f3821849e
antoine <antoine.quessy@uqtr.ca>
2026-06-10 12:35:42 -0400

    Tests + hamiltonien mixte

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from itertools import combinations
+from collections import deque
 
 np.set_printoptions(precision=5, linewidth=150, suppress=True)
 
@@ -15,53 +16,6 @@ class Ket:
     def __repr__(self):
         return self.name if self.name is not None else "Ket"
 
-
-def determinant_1kr(N):
-    """
-    Calcule le déterminant de la matrice de
-    transformation de base k vers r pour un
-    système à N sites comportant 1 électron.
-    Retourne la suite {1, -1, -i, -i, -1, 1, i, i}
-    *** Pour les N pairs : {-1, -i, 1, i}
-    *** Pour les N impairs : {1, -i, -1, i}
-    """
-    
-    m = np.zeros((N, N), dtype=complex)
-
-    for i in range(N):
-        for k in range(N):
-
-            m[i, k] = np.exp((2j*i*k*np.pi)/N)
-
-    m = (1/np.sqrt(N)) * m
-
-    det = np.linalg.det(m)
-
-    return det
-
-
-def determinant_1rk(N):
-    """
-    Calcule le déterminant de la matrice de
-    transformation de base r vers k pour un
-    système à N sites comportant 1 électron.
-    Retourne la suite {1, -1, i, i, -1, 1, -i, -i}
-    *** Pour les N pairs : {-1, i, 1, -i}
-    *** Pour les N impairs : {1, i, -1, -i}
-    """
-    
-    m = np.zeros((N, N), dtype=complex)
-
-    for i in range(N):
-        for k in range(N):
-
-            m[i, k] = np.exp((-2j*i*k*np.pi)/N)
-
-    m = (1/np.sqrt(N)) * m
-
-    det = np.linalg.det(m)
-
-    return det
 
 
 def metrique_rk(N):
@@ -112,26 +66,7 @@ def hamiltonian_r(N, t=1, U=2):
 
     H = np.zeros((dim, dim), dtype=float)
 
-    etats = []
-
-    for i in range(dim):
-
-        etat = []
-
-        for j in range(2*N):
-            etat.append(0)
-
-        compteur = 0
-
-        for j in range(2*N):
-            
-            if i - compteur >= 2**(2*N - (j+1)):
-
-                etat[j] = 1
-
-                compteur += 2**(2*N - (j+1))
-
-        etats.append(etat)
+    etats = [list(map(int, format(i, f'0{2*N}b'))) for i in range(dim)]
 
     index = {tuple(e): k for k, e in enumerate(etats)}
     
@@ -240,26 +175,7 @@ def hamiltonian_k(N, t=1, U=2):
 
     H = np.zeros((dim, dim), dtype=float)
 
-    etats = []
-
-    for i in range(dim):
-
-        etat = []
-
-        for j in range(2*N):
-            etat.append(0)
-
-        compteur = 0
-
-        for j in range(2*N):
-            
-            if i - compteur >= 2**(2*N - (j+1)):
-
-                etat[j] = 1
-
-                compteur += 2**(2*N - (j+1))
-
-        etats.append(etat)
+    etats = [list(map(int, format(i, f'0{2*N}b'))) for i in range(dim)]
 
     index = {tuple(e): k for k, e in enumerate(etats)}
     
@@ -641,37 +557,37 @@ def met_m(order, state1, state2, N):
 
             if state1_up[i] == 1:
 
-                r_up.append(i)
+                k_up.append(i)
 
             if state2_up[i] == 1:
 
-                k_up.append((2*np.pi*i)/N)
+                r_up.append((2*np.pi*i)/N)
 
             if state1_down[i] == 1:
 
-                r_down.append(i)
+                k_down.append(i)
 
             if state2_down[i] == 1:
 
-                k_down.append((2*np.pi*i)/N)
+                r_down.append((2*np.pi*i)/N)
 
         elif order == "kr":
 
             if state1_up[i] == 1:
 
-                k_up.append((2*np.pi*i)/N)
+                r_up.append((2*np.pi*i)/N)
 
             if state2_up[i] == 1:
 
-                r_up.append(i)
+                k_up.append(i)
 
             if state1_down[i] == 1:
 
-                k_down.append((2*np.pi*i)/N)
+                r_down.append((2*np.pi*i)/N)
 
             if state2_down[i] == 1:
 
-                r_down.append(i)
+                k_down.append(i)
         
 
     for i in range(n_up1):
@@ -679,7 +595,7 @@ def met_m(order, state1, state2, N):
 
             if order == "rk":
 
-                matrice_up[i, j] = np.exp(1j*k_up[i]*r_up[j])
+                matrice_up[i, j] = np.exp(1j*r_up[i]*k_up[j])
 
             elif order == "kr":
 
@@ -690,7 +606,7 @@ def met_m(order, state1, state2, N):
 
             if order == "rk":
 
-                matrice_down[i, j] = np.exp(1j*k_down[i]*r_down[j])
+                matrice_down[i, j] = np.exp(1j*r_down[i]*k_down[j])
 
             elif order == "kr":
 
@@ -731,97 +647,12 @@ def ground_energy(states, N, t=1, U=2):
                 H[i, j] = ham_k(states[i].vector, states[j].vector, N, t, U)
                 S[i, j] = met_k(states[i].vector, states[j].vector, N)
 
+
     eigenvalues, eigenvectors = sp.linalg.eigh(H, S)
 
     return eigenvalues, H, S
 
 
-
-def fundamental_energy(states, t=0.5, U=2):
-
-    key = lambda ket: tuple(ket.vector.tolist())
-
-    hamiltonian = {
-        key(ket_5r): Ket(U * ket_5r.vector - t * (ket_6r.vector + ket_9r.vector), "Hket_5r"),
-        key(ket_6r): Ket(-t * (ket_5r.vector + ket_10r.vector), "Hket_6r"),
-        key(ket_9r): Ket(-t * (ket_5r.vector + ket_10r.vector), "Hket_9r"),
-        key(ket_10r): Ket(U * ket_10r.vector - t * (ket_6r.vector + ket_9r.vector), "Hket_10r"),
-
-        key(ket_5k): Ket((2*t + U/2) * ket_5k.vector + U/2 * ket_10k.vector, "Hket_5k"),
-        key(ket_6k): Ket(U/2 * (ket_6k.vector + ket_9k.vector), "Hket_6k"),
-        key(ket_9k): Ket(U/2 * (ket_6k.vector + ket_9k.vector), "Hket_9k"),
-        key(ket_10k): Ket((-2*t + U/2) * ket_10k.vector + U/2 * ket_5k.vector, "Hket_10k"),
-    }
-
-    dim = len(states)
-    H = np.zeros((dim, dim), dtype=float)
-    S = np.zeros((dim, dim), dtype=float)
-
-    for i in range(dim):
-        for j in range(dim):
-            H[i, j] = np.dot(states[i].vector,
-                             hamiltonian[key(states[j])].vector)
-
-    for i in range(dim):
-        for j in range(dim):
-            S[i, j] = np.dot(states[i].vector, states[j].vector)
-
-    eigenvalues, eigenvectors = sp.linalg.eigh(H, S)
-
-    return eigenvalues
-
-
-ket_5r = Ket([1, 0, 0, 0], "ket_5r")
-ket_6r = Ket([0, 1, 0, 0], "ket_6r")
-ket_9r = Ket([0, 0, 1, 0], "ket_9r")
-ket_10r = Ket([0, 0, 0, 1], "ket_10r")
-
-ket_5k = Ket(1/2 * (ket_5r.vector - ket_6r.vector - ket_9r.vector + ket_10r.vector), "ket_5k")
-ket_6k = Ket(1/2 * (ket_5r.vector + ket_6r.vector - ket_9r.vector - ket_10r.vector), "ket_6k")
-ket_9k = Ket(1/2 * (ket_5r.vector - ket_6r.vector + ket_9r.vector - ket_10r.vector), "ket_9k")
-ket_10k = Ket(1/2 * (ket_5r.vector + ket_6r.vector + ket_9r.vector + ket_10r.vector), "ket_10k")
-
-##states_1 = [ket_5r, ket_6r, ket_9r, ket_10r]
-##
-##E_1 = fundamental_energy(states_1)
-##
-##E_1 = [float(round(e, 5)) for e in E_1]
-##
-##print(E_1)
-##
-##states_2 = [ket_5r, ket_10k, ket_10r]
-##
-##E_2 = fundamental_energy(states_2)
-##
-##E_2 = [float(round(e, 5)) for e in E_2]
-##
-##print(E_2)
-
-##states = [ket_5r, ket_6r, ket_9r, ket_10r, ket_5k, ket_6k, ket_9k, ket_10k]
-##
-##for a, b, c in combinations(states, 3):
-##    etats = [a, b, c]
-##
-##    E = fundamental_energy(etats)
-##    E = [float(round(e, 5)) for e in E_2]
-##
-##    print([s.name for s in etats])
-##    print(E)
-
-##M = hamiltonian_r(4)
-##
-##for i in range(len(M)):
-##
-##    row = M[i]
-##
-##    for j in range(len(row)):
-##
-##        if row[j] != 0:
-##
-##            print("ket", i,". ket",  j, " = ", row[j])
-
-
-from collections import deque
 
 def connected_component(H, start):
 
@@ -842,6 +673,7 @@ def connected_component(H, start):
     return visited
 
 
+
 def all_components(H):
     n = H.shape[0]
     visited = set()
@@ -856,94 +688,173 @@ def all_components(H):
     return components
 
 
-##H_r = hamiltonian_r(4)
-##
-##comps_r = all_components(H_r)
-##
-##fund_r = []
-##
-##for c in comps_r:
-##
-##    print(c)
-##
-##    S_sorted = sorted(c)
-##    H_sub = H_r[np.ix_(S_sorted, S_sorted)]
-##
-##    eigvals = []
-##
-##    for val in np.linalg.eigvals(H_sub): 
-##        eigvals.append(float(round(val.real, 5)))
-##
-##    for el in eigvals:
-##        fund_r.append(el)
-##
-##    print(eigvals)
-##
-##print(sorted(fund_r))
-##
-##    
-##H_k = hamiltonian_k(4)
-##
-##comps_k = all_components(H_k)
-##
-##fund_k = []
-##
-##for c in comps_k:
-##
-##    print(c)
-##
-##    S_sorted = sorted(c)
-##    H_sub = H_k[np.ix_(S_sorted, S_sorted)]
-##
-##    eigvals = []
-##
-##    for val in np.linalg.eigvals(H_sub): 
-##        eigvals.append(float(round(val.real, 5)))
-##
-##    for el in eigvals:
-##        fund_k.append(el)
-##
-##    print(eigvals)
-##
-##print(sorted(fund_k))
-
-etats = [51, 53, 54, 57, 58, 60, 83, 85, 86, 89, 90, 92, 99, 101, 102, 105, 106, 108, 147, 149, 150, 153, 154, 156, 163, 165, 166, 169, 170, 172, 195, 197, 198, 201, 202, 204]
-
-states_r = []
-states_k = []
-
-states = []
-
-for element in etats:
-    ket_r = Ket(list(map(int, format(element, f'0{8}b'))), "r", str(element) + "r")
-    ket_k = Ket(list(map(int, format(element, f'0{8}b'))), "k", str(element) + "k")
-
-    states.append(ket_r)
-    states_r.append(ket_r)
-    states.append(ket_k)
-    states_k.append(ket_k)
 
 
-def test(n, t=1, U=2):
+## Test 4-sites : sous-espaces r/k et energies fondamentales associees
 
-    ensemble_test = states_r
 
-    ensemble_test[2] = states_k[2]
-
-    E0 = min(ground_energy(ensemble_test, 4, t, U)[0])
-
-##    for etats in combinations(states, n):
-##        E = ground_energy(list(etats), 4, t, U)
-##        E = [float(round(e, 5)) for e in E]
-##
-##        if min(E) == E0:
-
-##            print([s.name for s in etats])
-
-    print(ground_energy(ensemble_test, 4, t, U)[2])
-
-print(test(1))
+#H_r = hamiltonian_r(4)
+#
+#comps_r = all_components(H_r)
+#
+#fund_r = []
+#
+#for c in comps_r:
+#
+#    print(c)
+#
+#    S_sorted = sorted(c)
+#    H_sub = H_r[np.ix_(S_sorted, S_sorted)]
+#
+#    eigvals = []
+#
+#    for val in np.linalg.eigvals(H_sub): 
+#        eigvals.append(float(round(val.real, 5)))
+#
+#    for el in eigvals:
+#        fund_r.append(el)
+#
+#    print(eigvals)
+#
+#print(sorted(fund_r))
+#
+#    
+#H_k = hamiltonian_k(4)
+#
+#comps_k = all_components(H_k)
+#
+#fund_k = []
+#
+#for c in comps_k:
+#
+#    print(c)
+#
+#    S_sorted = sorted(c)
+#    H_sub = H_k[np.ix_(S_sorted, S_sorted)]
+#
+#    eigvals = []
+#
+#    for val in np.linalg.eigvals(H_sub): 
+#        eigvals.append(float(round(val.real, 5)))
+#
+#    for el in eigvals:
+#        fund_k.append(el)
+#
+#    print(eigvals)
+#
+#print(sorted(fund_k))
 
 
 
+## Test 4-sites : etat mixte de 6 etats (2r, 4k)
+
+
+#etats = [51, 53, 54, 57, 58, 60, 83, 85, 86, 89, 90, 92, 99, 101, 102, 105, 106, 108, 147, 149, 150, 153, 154, 156, 163, 165, 166, 169, 170, 172, 195, 197, 198, 201, 202, 204]
+#
+#etats1 = [51, 60, 90, 102, 105, 150, 153, 165, 195, 204]
+#etats2 = [53, 83, 92, 106, 154, 166, 169, 197]
+#etats3 = [54, 57, 85, 99, 108, 147, 156,  170, 198, 201]
+#etats4 = [58, 86, 89, 101, 149, 163, 172, 202]
+#
+#states_r1 = []
+#states_r2 = []
+#states_r3 = []
+#states_r4 = []
+#states_k1 = []
+#states_k2 = []
+#states_k3 = []
+#states_k4 = []
+#
+#states = []
+#
+#for element in etats1:
+#    ket_r = Ket(list(map(int, format(element, f'0{8}b'))), "r", str(element) + "r")
+#    ket_k = Ket(list(map(int, format(element, f'0{8}b'))), "k", str(element) + "k")
+#
+#    states_r1.append(ket_r)
+#    states_k1.append(ket_k)
+#
+#for element in etats2:
+#    ket_r = Ket(list(map(int, format(element, f'0{8}b'))), "r", str(element) + "r")
+#    ket_k = Ket(list(map(int, format(element, f'0{8}b'))), "k", str(element) + "k")
+#
+#    states_r2.append(ket_r)
+#    states_k2.append(ket_k)
+#
+#for element in etats3:
+#    ket_r = Ket(list(map(int, format(element, f'0{8}b'))), "r", str(element) + "r")
+#    ket_k = Ket(list(map(int, format(element, f'0{8}b'))), "k", str(element) + "k")
+#
+#    states_r3.append(ket_r)
+#    states_k3.append(ket_k)
+#
+#for element in etats4:
+#    ket_r = Ket(list(map(int, format(element, f'0{8}b'))), "r", str(element) + "r")
+#    ket_k = Ket(list(map(int, format(element, f'0{8}b'))), "k", str(element) + "k")
+#
+#    states_r4.append(ket_r)
+#    states_k4.append(ket_k)
+#
+#
+#
+#def test_4(t=1, U=2):
+#
+#    for etats_r in combinations(states_r1, 2):
+#
+#        states = []
+#    
+#        for e in etats_r:
+#            states.append(e)
+#    
+#        for etats_k in combinations(states_k1, 4):
+#
+#            new_states = states.copy()
+#        
+#            for e in etats_k:
+#                new_states.append(e)
+#
+#            E0 = min(ground_energy(new_states, 4, 1, 2)[0])
+#
+#            if E0 < -2.8:
+#
+#                print([s.name for s in new_states])
+#                print(round(E0, 5))
+#
+#
+#test_4()
+
+
+
+## Test 2-sites : 3 etats
+
+
+#etats = [5, 6, 9, 10]
+#
+#states_r = []
+#states_k = []
+#states = []
+#
+#for element in etats:
+#    ket_r = Ket(list(map(int, format(element, f'0{4}b'))), "r", str(element) + "r")
+#    ket_k = Ket(list(map(int, format(element, f'0{4}b'))), "k", str(element) + "k")
+#
+#    states.append(ket_r)
+#    states_r.append(ket_r)
+#    states.append(ket_k)
+#    states_k.append(ket_k)
+#
+#
+#def test_2(t=1, U=2):
+#
+#    for etats in combinations(states, 3):
+#        
+#        E0 = min(ground_energy(list(etats), 2, t, U)[0])
+#
+#        if E0 < -3.12:
+#
+#            print([s.name for s in etats])
+#            print(round(E0, 5))
+#
+#
+#test_2() 
 
